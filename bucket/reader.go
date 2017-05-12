711216411677ba1a2d3bde04830e76981fdafc4d
Stephen E Ingram <singram@openwhere.com>
2017-05-12 15:09:36 -0400

    Added BucketFile and S3FileReader

--- bucket/reader.go
+++ bucket/reader.go
@@ -6,6 +6,8 @@ package bucket
 
 import (
 	"fmt"
+	"io"
+	"path"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -15,12 +17,14 @@ import (
 
 const (
 	ISO8601FORMAT = "20060102T150405Z"
-	VERSION       = "0.1"
 )
 
 func mineAllObjects(region, bucket, path string) ([]*s3.Object, error) {
 	svc := s3.New(session.New(), &aws.Config{Region: aws.String(region)})
 	input := &s3.ListObjectsInput{Bucket: aws.String(bucket), Prefix: aws.String(path)}
+	if path == "/" {
+		input = &s3.ListObjectsInput{Bucket: aws.String(bucket)}
+	}
 	output, err := svc.ListObjects(input)
 	if err != nil {
 		return nil, err
@@ -53,7 +57,7 @@ func mineAllObjects(region, bucket, path string) ([]*s3.Object, error) {
 	return retval, nil
 }
 
-func parsePath(dir string) (bucket, prefix string) {
+func ParsePath(dir string) (bucket, prefix string) {
 	var parts []string
 	if strings.HasPrefix(dir, "s3://") {
 		if strings.HasSuffix(dir, "/") {
@@ -78,12 +82,30 @@ func parsePath(dir string) (bucket, prefix string) {
 }
 
 type BucketFile struct {
-	Name         string
+	Bucket       string
+	Key          string
 	LastModified string
+	Region       string
+	Size         int64
+}
+
+func NewBucketFile(region, bucket, key, lastModified string, size int64) *BucketFile {
+	return &BucketFile{
+		Region:       region,
+		Bucket:       bucket,
+		Key:          key,
+		LastModified: lastModified,
+		Size:         size}
+}
+
+func (bf *BucketFile) Paths() ([]string, string) {
+	dir, file := path.Split(bf.Key)
+	dirs := strings.Split(dir, "/")
+	return dirs, file
 }
 
 func ReadBucket(region, dir string) ([]*BucketFile, error) {
-	bucket, prefix := parsePath(dir)
+	bucket, prefix := ParsePath(dir)
 	objects, err := mineAllObjects(region, bucket, prefix)
 	if err != nil {
 		return []*BucketFile{}, err
@@ -96,8 +118,107 @@ func ReadBucket(region, dir string) ([]*BucketFile, error) {
 		// The directory will match, so filter it out
 		if filename != dirName {
 			dateTime := fmt.Sprintf("%s", object.LastModified.UTC().Format(ISO8601FORMAT))
-			retval = append(retval, &BucketFile{Name: aws.StringValue(object.Key), LastModified: dateTime})
+			retval = append(retval, NewBucketFile(region, bucket, aws.StringValue(object.Key), dateTime, *object.Size))
 		}
 	}
 	return retval, nil
+}
+
+func ListBucketStructure(region, bucket string) (*DirItem, error) {
+	if !strings.HasSuffix(bucket, "/") {
+		bucket = bucket + "/"
+	}
+	bfs, err := ReadBucket(region, bucket)
+	if err != nil {
+		return nil, err
+	}
+	root := NewDirItem("/")
+	for _, bf := range bfs {
+		rootDir := root
+		paths, _ := bf.Paths()
+		for _, p := range paths {
+			if p != "" {
+				if nd, ok := rootDir.Children[p]; !ok {
+					newDir := NewDirItem(p)
+					rootDir.Add(newDir)
+					rootDir = newDir
+				} else {
+					rootDir = nd
+				}
+			}
+		}
+		rootDir.AddKey(bf)
+	}
+	return root, nil
+}
+
+type DirItem struct {
+	Name     string
+	Children map[string]*DirItem
+	Keys     []*BucketFile
+	Size     int64
+}
+
+func NewDirItem(name string) *DirItem {
+	return &DirItem{
+		Name:     name,
+		Children: make(map[string]*DirItem),
+		Keys:     make([]*BucketFile, 0, 32),
+		Size:     0,
+	}
+}
+
+func (di *DirItem) Contains(name string) bool {
+	_, ok := di.Children[name]
+	return ok
+}
+
+func (di *DirItem) Add(item *DirItem) {
+	if _, ok := di.Children[item.Name]; !ok {
+		di.Children[item.Name] = item
+	}
+}
+
+func (di *DirItem) AddKey(item *BucketFile) {
+	di.Keys = append(di.Keys, item)
+	di.Size += item.Size
+}
+
+type DirInfo struct {
+	Level int
+	Keys  int
+	Name  string
+	Size  int64
+}
+
+func (di *DirItem) Print(lvl int, ch chan *DirInfo) {
+	ch <- &DirInfo{Level: lvl, Keys: len(di.Keys), Name: di.Name, Size: di.Size}
+	for _, v := range di.Children {
+		v.Print(lvl+1, ch)
+	}
+}
+
+type S3FileReader struct {
+	sectionReader *io.SectionReader
+}
+
+func (sfr *S3FileReader) Read(p []byte) (n int, err error) {
+	return sfr.sectionReader.Read(p)
+}
+
+func (sfr *S3FileReader) ReadAt(p []byte, off int64) (n int, err error) {
+	return sfr.sectionReader.ReadAt(p, off)
+}
+
+func (sfr *S3FileReader) Size() int64 {
+	return sfr.sectionReader.Size()
+}
+
+// S3FileReader is completely reusable
+// This keeps requests down while allowing multiple readers
+// to access it for determining types
+func (bf *BucketFile) Stream() *S3FileReader {
+	svc := s3.New(session.New(), &aws.Config{Region: aws.String(bf.Region)})
+	return &S3FileReader{
+		sectionReader: io.NewSectionReader(NewChunkReader(bf, svc), 0, bf.Size)}
 }
